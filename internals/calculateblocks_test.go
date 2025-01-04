2459cde8eba2e16db27f17ac3ee83700181b1e85
andyosyndoh <andyovvo8@gmail.com>
2025-01-04 22:22:08 +0300

    chore: added major minor test file

--- internals/calculateblocks_test.go
+++ internals/calculateblocks_test.go
@@ -50,44 +50,98 @@ func Test_max(t *testing.T) {
 }
 
 func Test_isHidden(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Hidden file",
+			args: args{name: ".hiddenfile"},
+			want: true,
+		},
+		{
+			name: "Non-hidden file",
+			args: args{name: "visiblefile.txt"},
+			want: false,
+		},
+		{
+			name: "File with dot in the middle",
+			args: args{name: "file.with.dots.txt"},
+			want: false,
+		},
+		{
+			name: "Single dot",
+			args: args{name: "."},
+			want: true,
+		},
+		{
+			name: "Double dot",
+			args: args{name: ".."},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isHidden(tt.args.name); got != tt.want {
+				t.Errorf("isHidden() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_majorMinor(t *testing.T) {
     type args struct {
-        name string
+        rdev uint64
     }
     tests := []struct {
-        name string
-        args args
-        want bool
+        name  string
+        args  args
+        want  uint64
+        want1 uint64
     }{
         {
-            name: "Hidden file",
-            args: args{name: ".hiddenfile"},
-            want: true,
+            name:  "Device number 0",
+            args:  args{rdev: 0},
+            want:  0,
+            want1: 0,
         },
         {
-            name: "Non-hidden file",
-            args: args{name: "visiblefile.txt"},
-            want: false,
+            name:  "Device number 8",
+            args:  args{rdev: 8},
+            want:  0,
+            want1: 8,
         },
         {
-            name: "File with dot in the middle",
-            args: args{name: "file.with.dots.txt"},
-            want: false,
+            name:  "Device number 513",
+            args:  args{rdev: 513},
+            want:  2,
+            want1: 1,
         },
         {
-            name: "Single dot",
-            args: args{name: "."},
-            want: true,
+            name:  "Device number 66305",
+            args:  args{rdev: 66305},
+            want:  259,
+            want1: 1,
         },
         {
-            name: "Double dot",
-            args: args{name: ".."},
-            want: true,
+            name:  "Large device number",
+            args:  args{rdev: 18446744073709551615},
+            want:  4095,
+            want1: 1048575,
         },
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
-            if got := isHidden(tt.args.name); got != tt.want {
-                t.Errorf("isHidden() = %v, want %v", got, tt.want)
+            got, got1 := majorMinor(tt.args.rdev)
+            if got != tt.want {
+                t.Errorf("majorMinor() got = %v, want %v", got, tt.want)
+            }
+            if got1 != tt.want1 {
+                t.Errorf("majorMinor() got1 = %v, want %v", got1, tt.want1)
             }
         })
     }
